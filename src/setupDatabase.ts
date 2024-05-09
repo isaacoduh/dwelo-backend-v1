b5b1d1203efc69be9b6373575a3faf740fac7356
Isaac Oduh <isaacoduh@IOs-MacBook-Pro.local>
2024-05-09 16:59:46 +0100

    Initialize

---
+++ src/setupDatabase.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import Logger from "bunyan";
+import { config } from "./config";
+import { redisConnection } from "./utils/redis/redis.connection";
+
+const log: Logger = config.createLogger("setupDatabase");
+
+export default () => {
+  const connect = () => {
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info("Database connected successfully!");
+        redisConnection.connect();
+      })
+      .catch((error) => {
+        log.error("Error connecting to database", error);
+        return process.exit(1);
+      });
+  };
+  connect();
+
+  mongoose.connection.on("disconnected", connect);
+};
---
+++ src/setupServer.ts
@@ -0,0 +1,73 @@
+import Logger from "bunyan";
+import http from "http";
+import cors from "cors";
+import { createClient } from "redis";
+import { createAdapter } from "@socket.io/redis-adapter";
+import "express-async-errors";
+import { Server as SocketServer } from "socket.io";
+
+import {
+  Application,
+  json,
+  urlencoded,
+  Response,
+  Request,
+  NextFunction,
+} from "express";
+import { config } from "./config";
+
+const SERVER_PORT = 5600;
+const log: Logger = config.createLogger("server");
+
+export class Server {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.startServer(this.app);
+  }
+
+  private async startServer(app: Application): Promise<void> {
+    if (!config.JWT_TOKEN) {
+      throw new Error("JWT_TOKEN must be provided!");
+    }
+
+    try {
+      const httpServer: http.Server = new http.Server(app);
+      const socketIO: SocketServer = await this.createSocketIO(httpServer);
+      this.startHttpServer(httpServer);
+      this.socketIOConnections(socketIO);
+    } catch (error) {
+      log.error(error);
+    }
+  }
+
+  private async createSocketIO(httpServer: http.Server): Promise<SocketServer> {
+    const io: SocketServer = new SocketServer(httpServer, {
+      cors: {
+        origin: config.CLIENT_URL,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      },
+    });
+    const pubClient = createClient({ url: config.REDIS_HOST });
+    const subClient = pubClient.duplicate();
+    await Promise.all([pubClient.connect(), subClient.connect()]);
+    io.adapter(createAdapter(pubClient, subClient));
+    return io;
+  }
+
+  private startHttpServer(httpServer: http.Server): void {
+    log.info(`Worker with process id of ${process.pid} has started`);
+    log.info(`Server has started with process ${process.pid}`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Server running on port ${SERVER_PORT}`);
+    });
+  }
+
+  private socketIOConnections(io: SocketServer): void {
+    //TODO: Implement socket io connections here
+  }
+}
